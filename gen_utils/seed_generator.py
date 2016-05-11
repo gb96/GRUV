28201edcd5092b0f896360a4193efa3542ab1827
Wildream <wildreamtech@gmail.com>
2016-05-11 03:28:22 +0200

    Switching to Keras 1.0; Some PEP8-friendly beautifications

--- gen_utils/seed_generator.py
+++ gen_utils/seed_generator.py
@@ -1,11 +1,16 @@
 import numpy as np
 
-#A very simple seed generator
-#Copies a random example's first seed_length sequences as input to the generation algorithm
+
+# A very simple seed generator
+# Copies a random example's first seed_length sequences
+# as input to the generation algorithm
+
+
 def generate_copy_seed_sequence(seed_length, training_data):
-	num_examples = training_data.shape[0]
-	example_len = training_data.shape[1]
-	randIdx = np.random.randint(num_examples, size=1)[0]
-	randSeed = np.concatenate(tuple([training_data[randIdx + i] for i in xrange(seed_length)]), axis=0)
-	seedSeq = np.reshape(randSeed, (1, randSeed.shape[0], randSeed.shape[1]))
-	return seedSeq
+    num_examples = training_data.shape[0]
+    rand_idx = np.random.randint(num_examples, size=1)[0]
+    rand_seed = np.concatenate(tuple([training_data[rand_idx + i]
+                                      for i in xrange(seed_length)]), axis=0)
+    seed_seq = np.reshape(rand_seed, (1, rand_seed.shape[0],
+                                      rand_seed.shape[1]))
+    return seed_seq
--- gen_utils/sequence_generator.py
+++ gen_utils/sequence_generator.py
@@ -1,29 +1,33 @@
 import numpy as np
 
-#Extrapolates from a given seed sequence
+
+# Extrapolates from a given seed sequence
+
+
 def generate_from_seed(model, seed, sequence_length, data_variance, data_mean):
-	seedSeq = seed.copy()
-	output = []
+    seed_seq = seed.copy()
+    output = []
 
-	#The generation algorithm is simple:
-	#Step 1 - Given A = [X_0, X_1, ... X_n], generate X_n + 1
-	#Step 2 - Concatenate X_n + 1 onto A
-	#Step 3 - Repeat MAX_SEQ_LEN times
-	for it in xrange(sequence_length):
-		seedSeqNew = model._predict(seedSeq) #Step 1. Generate X_n + 1
-		#Step 2. Append it to the sequence
-		if it == 0:
-			for i in xrange(seedSeqNew.shape[1]):
-				output.append(seedSeqNew[0][i].copy())
-		else:
-			output.append(seedSeqNew[0][seedSeqNew.shape[1]-1].copy()) 
-		newSeq = seedSeqNew[0][seedSeqNew.shape[1]-1]
-		newSeq = np.reshape(newSeq, (1, 1, newSeq.shape[0]))
-		seedSeq = np.concatenate((seedSeq, newSeq), axis=1)
+    # The generation algorithm is simple:
+    # Step 1 - Given A = [X_0, X_1, ... X_n], generate X_n + 1
+    # Step 2 - Concatenate X_n + 1 onto A
+    # Step 3 - Repeat MAX_SEQ_LEN times
+    for it in xrange(sequence_length):
+        seed_seq_new = model._predict(seed_seq)  # Step 1. Generate X_n + 1
+        # Step 2. Append it to the sequence
+        if it == 0:
+            for i in xrange(seed_seq_new.shape[1]):
+                output.append(seed_seq_new[0][i].copy())
+            else:
+                output.append(seed_seq_new[0][seed_seq_new.shape[1] - 1].copy())
+            new_seq = seed_seq_new[0][seed_seq_new.shape[1] - 1]
+            new_seq = np.reshape(new_seq, (1, 1, new_seq.shape[0]))
+            seed_seq = np.concatenate((seed_seq, new_seq), axis=1)
 
-	#Finally, post-process the generated sequence so that we have valid frequencies
-	#We're essentially just undo-ing the data centering process
-	for i in xrange(len(output)):
-		output[i] *= data_variance
-		output[i] += data_mean
-	return output
+    # Finally, post-process the generated sequence
+    # so that we have valid frequencies
+    # We're essentially just undo-ing the data centering process
+    for i in xrange(len(output)):
+        output[i] *= data_variance
+        output[i] += data_mean
+    return output
